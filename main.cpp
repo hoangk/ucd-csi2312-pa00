e97186229827679f29458df4043119305a98cc2c
hoangk <khoaq.hoang@gmail.com>
2015-10-05 23:24:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,7 @@ using namespace std;
 
 int main() {
 
-    Clustering::Point p1[1,2,3], p2[0,0,0,0];
-
-    double d;
-    d = distanceTo( p1, p2);
+   
 
     cout << "Hello, World!" << endl;
     return 0;
